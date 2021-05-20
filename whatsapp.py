9405399271eb809d7edd6b20ac5240b091924c9a
ShubhamGarg01 <72695387+ShubhamGarg01@users.noreply.github.com>
2021-05-20 13:29:10 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,14 @@
+import pywhatkit
+import csv
+from datetime import datetime
+print("Enter the number")
+message = ["Hello","Welcome in Tech Tomatoes","Are u looking for intership"]
+for i in range(len(message)):
+        print(f"{i} - {message[i]}")
+msg = message[int(input())]        
+with open("FilePath.csv","r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        time = datetime.now()
+        pywhatkit.sendwhatmsg(f"+91-{row[1]}",msg,time.hour,time.minute+1,5)
+        
